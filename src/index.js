a61c556bb5ec3ec727cd644b8117410af0fc5b29
Salah Eddine Bachir-Cherif <gs_bachircherif@esi.dz>
2018-10-16 02:45:17 +0100

    Setting up Redux to the project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./reducers";
+const store = createStore(rootReducer);
 
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
