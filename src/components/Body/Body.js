097f6691b1edf4a2a13e8a1aa44a7743c70de741
Tyler Malone <tyler@pylon.com>
2018-10-26 14:36:21 -0400

    links to relevant documentation

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -6,6 +6,13 @@ const Body = props => {
     <div className="bg-light">
       <div className="body">
         <p className="tx-dark">{props.text}</p>
+        <p className="tx-dark">These links may be helpful:</p>
+        <a href="https://reactjs.org/docs/getting-started.html">
+          React Documention
+        </a>
+        <a href="https://webpack.js.org/concepts/">Webpack Documention</a>
+        <a href="https://babeljs.io/docs/en/">Babel Documention</a>
+        <a href="https://jestjs.io/docs/en/getting-started">Jest Documention</a>
       </div>
     </div>
   );
